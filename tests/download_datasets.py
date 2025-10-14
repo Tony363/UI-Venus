98a414011dbd04d35c91a922f2f7a7519060d14f
Tony Siu <pysolver33@gmail.com>
2025-10-14 00:43:34 +0000

    cleanup

--- tests/download_datasets.py
+++
@@ -1,7 +0,0 @@
-from datasets import load_dataset
-
-# Login using e.g. `huggingface-cli login` to access this dataset
-ds = load_dataset("likaixin/ScreenSpot-v2-variants", cache_dir="ScreenSpot-v2-variants/")
-ds = load_dataset("likaixin/ScreenSpot-Pro",cache_dir="Screenspot-pro/")
-
-
