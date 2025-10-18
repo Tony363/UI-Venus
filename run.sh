d88410a9d58605f30e48d3a1055427de001efa80
Tony Siu <pysolver33@gmail.com>
2025-10-18 10:16:11 +0000

    distributed gpus

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 cd /teamspace/studios/this_studio/UI-Venus
+
+# Default tensor parallelism to a single GPU when not explicitly set.
+export UI_VENUS_TP="${UI_VENUS_TP:-1}"
+
 uvicorn autonomous_api:app --host 0.0.0.0 --port 8000
